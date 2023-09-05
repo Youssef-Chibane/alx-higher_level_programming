cf7b482f47cf58f54f0a8e91c29f97b019fd70c5
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-09-06 00:00:49 +0100

    format!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i / 10 < i % 10:
-        print(f"{i:02d}", end=", ")
+        print("{:02d}".format(i), end=", ")
         if i == 89:
-            print(f"{i:02d}")
+            print("{:02d}".format(i))
