d6c8a318f4beb155051407a2695c21372fbd8d78
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-09-05 21:58:35 +0100

    fixedls!

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_d = abs(number) % 10
+
+# get the absolute value of a number
+if number >= 0:
+    check_n = number
+else:
+    check_n = number * -1
+
+# get the last digit of a number
+last_d = check_n % 10
+
+# check the given conditions
 if last_d > 5:
     print(f"Last digit of {number} is {last_d} and is greater than 5")
 elif last_d == 0:
