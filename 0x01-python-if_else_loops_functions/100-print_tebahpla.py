b205cfbf1ed4b28642e11863b53cd06b4e482a99
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-09-06 01:49:20 +0100

    14 fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{}{}".format(chr(i+32), chr(i)), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        print(chr(i).lower(), end='')
+    else:
+        print(chr(i).upper(), end='')
