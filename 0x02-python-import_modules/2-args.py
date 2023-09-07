3c476097b81356045ac28496dd942a007c139761
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-09-08 00:46:56 +0100

    fixgit add 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
+if __name__ == '__main__':
+    args_number = len(argv)
 
-args_number = len(argv)
+    # print number of arguments.
+    if args_number - 1 == 0:
+        print("{:d} arguments.".format(args_number - 1))
+    elif args_number - 1 == 1:
+        print("{:d} argument:".format(args_number - 1))
+    else:
+        print("{:d} arguments:".format(args_number - 1))
 
-# print number of arguments.
-if args_number - 1 == 0:
-    print("{:d} arguments.".format(args_number - 1))
-elif args_number - 1 == 1:
-    print("{:d} argument:".format(args_number - 1))
-else:
-    print("{:d} arguments:".format(args_number - 1))
-
-# print the argument with their indexes
-for index, argument in enumerate(argv):
-    if index != 0:
-        print("{:d}: {:s}".format(index, argument))
+    # print the argument with their indexes
+    for index, argument in enumerate(argv):
+        if index != 0:
+            print("{:d}: {:s}".format(index, argument))
