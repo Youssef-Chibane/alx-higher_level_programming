03c6415dc84a7b9b04dea5bcd06292a00d9a2913
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-09-26 00:43:26 +0100

    pycode again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    
 
     for i in range(x):
         try:
@@ -9,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-        
+
     print()
     return count
