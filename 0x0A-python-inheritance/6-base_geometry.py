cb2db7409e708162affc98409b5891efc82c90d6
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-04 23:36:01 +0100

    task 06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+basegeometry.py - A Python script defining a base class 'BaseGeometry'.
+
+"""
+
+
+class BaseGeometry:
+    """
+    A Python script defining a base class 'BaseGeometry'.
+
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
