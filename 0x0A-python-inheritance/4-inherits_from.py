c940f971c68807801c04ab372eafefc9fa5ed414
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-04 23:21:54 +0100

    task 04

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+classcheck.py - A Python script defining a function 'inherits_from'
+to check if an object inherits from a specified class.
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if the given object inherits from the specified class.
+
+    """
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
