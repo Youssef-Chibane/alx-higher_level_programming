76a8e140e74c9e8319aa3b6c785badac7dec4471
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-05 01:43:28 +0100

    pycodestyle again

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,12 @@ MyInt class, a subclass of int.
 This class overrides the equality (__eq__) and inequality (__ne__) methods
 to provide custom behavior for comparing MyInt objects with other integers.
 
-Methods:
+"""
+
+
+class MyInt(int):
+    """
+    Methods:
      __eq__(self, other):
         Compares the value of MyInt with another integer
         using a non-standard equality check.
@@ -14,10 +19,7 @@ Methods:
     __ne__(self, other):
         Compares the value of MyInt with another integer
         using a non-standard inequality check.
-"""
-
-
-class MyInt(int):
+    """
     def __eq__(self, other):
         return int(str(self)) != other
 
