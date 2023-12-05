127994a5621d6cfb7f1f50f394a3a154ebd6369e
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-05 22:05:37 +0100

    pycodestyle fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,9 +8,10 @@ Write the specified text to a file with the given filename.
 def write_file(filename="", text=""):
     """
     Parameters:
-    - filename (str): The name of the file to write to. If the file already exists,
-    its content will be overwritten. If the file does not exist,
-    a new file with the specified filename will be created.
+    - filename (str): The name of the file to write to.
+    If the file already exists, its content will be overwritten.
+    If the file does not exist, a new file with the
+    specified filename will be created.
     - text (str): The text to be written to the file.
 
     """
