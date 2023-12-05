0cd5735eb058530cffe49cbe756afd610709ba84
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-05 21:42:55 +0100

    req?

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""
+read_file function reads and prints the contents of a file.
+
+"""
+
 
 def read_file(filename=""):
     """
-    read_file function reads and prints the contents of a file.
-
+    Parameters: filename (str): The name of the file to be read.
+    Default is an empty string,
+    in which case the function reads a file named 'my_file_0.txt'.
     """
+
     with open(filename, 'r') as f:
         for line in f:
             print(line, end='')
