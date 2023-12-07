ba26652536dfd9d0cc55d7fde2af02cf08c927e6
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-07 01:24:00 +0100

    anyway

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a class Student"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance of the Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary description with simple data structure"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for i in json:
+            self.__dict__[i] = json[i]
