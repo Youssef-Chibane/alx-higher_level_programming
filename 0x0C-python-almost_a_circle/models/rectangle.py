9604ab3155f2a28673809d6ef0fe8b7f5fa82b47
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-12-11 16:22:48 +0100

    task 07

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,8 +99,10 @@ class Rectangle(Base):
         Displays the rectangle by printing it
         to stdout using the character '#'.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
